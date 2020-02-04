fcf3e1c670d22eebca77ba0a2576a30438ef9802
Felipe Tomé <felipe.tome@idace.ce.gov.br>
2020-02-04 17:09:01 -0300

    add novos arquivos

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '5edd99e25f75c21144df4b945e0749fc',
+  espiresIn: '7d',
+};
--- src/config/database.js
+++ src/config/database.js
@@ -3,7 +3,7 @@ module.exports = {
   host: 'localhost',
   username: 'postgres',
   password: 'docker',
-  databese: 'teste',
+  databese: 'root',
   define: {
     timestamps: true,
     underscored: true,
